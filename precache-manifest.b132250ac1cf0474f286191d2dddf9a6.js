322258ac9710883af242c589760ba0f8cf227a19
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-22 11:56:13 +0200

    Updates

---
+++ precache-manifest.b132250ac1cf0474f286191d2dddf9a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51a3638130f45e722a3b3f10a79f4cf",
+    "url": "/FullStack-TechGoods/index.html"
+  },
+  {
+    "revision": "ec0e8cbd1571e15c3855",
+    "url": "/FullStack-TechGoods/static/css/main.8030f5c5.chunk.css"
+  },
+  {
+    "revision": "cf0eecefdce917ca5492",
+    "url": "/FullStack-TechGoods/static/js/2.93836d97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FullStack-TechGoods/static/js/2.93836d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0e8cbd1571e15c3855",
+    "url": "/FullStack-TechGoods/static/js/main.725c207d.chunk.js"
+  },
+  {
+    "revision": "2011793ac72b22a24e50",
+    "url": "/FullStack-TechGoods/static/js/runtime-main.fce9ddfb.js"
+  },
+  {
+    "revision": "6243b827d82209031cd4054927394bca",
+    "url": "/FullStack-TechGoods/static/media/cart.6243b827.svg"
+  }
+]);
